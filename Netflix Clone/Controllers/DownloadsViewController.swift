ca4fc91cb9b21ae8568bab78e5d002f64e197f82
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-30 09:30:31 +0700

    feat: add TabBarController

---
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by Jan Poonthong on 30/9/2567 BE.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+
+}
---
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Jan Poonthong on 30/9/2567 BE.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
