7a728b9e8a2b70b370476b71ae9c545f8e4c905b
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-30 09:08:45 +0700

    chore: initial iOS application setup

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Jan Poonthong on 30/9/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
